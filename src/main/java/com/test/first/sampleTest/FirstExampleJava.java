502e3fea7faeb57b5a1abd1b8231751a15e81609
sugithanandan <sugithanandan@gmail.com>
2016-11-27 05:40:33 +0530

    updated pro

---
+++ src/main/java/com/test/first/sampleTest/FirstExampleJava.java
@@ -0,0 +1,15 @@
+package com.test.first.sampleTest;
+
+public class FirstExampleJava {
+	
+	public FirstExampleJava()
+	{
+		System.out.println("Hi Am Constructor from First Example Java");
+	}
+	
+	public void sampletest()
+	{
+		System.out.println(" Hi i am first method");
+	}
+
+}
